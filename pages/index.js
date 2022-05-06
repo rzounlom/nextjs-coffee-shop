567a34b20b4aa607bc118719f4f05f0f737837f3
Romaric Zounlome <rzounlom@gmail.com>
2022-05-06 08:53:54 -0400

    add banner component; update styles

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Banner from "../components/banner";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    alert("Clicked");
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +15,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoissuer</h1>
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
       </main>
     </div>
   );
